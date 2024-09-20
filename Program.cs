7056094566f280f12cffcde404fb8b14befd1cca
sureshnewdevdev <45984902+sureshnewdevdev@users.noreply.github.com>
2024-09-20 18:06:11 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace CoreFunda
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int a, b;
+
+            string inputA = string.Empty;
+            string inputB = string.Empty;
+
+            Console.WriteLine("Please enter a value for A");
+            inputA = Console.ReadLine();
+
+            Console.WriteLine("Please enter a value for B");
+            inputB = Console.ReadLine();
+
+            a = int.Parse(inputA);
+            b= int.Parse(inputB);
+            
+
+            int ans = a+ b;
+            Console.WriteLine("Answer = " + ans);
+            Console.ReadLine();
+        }
+    }
+}
